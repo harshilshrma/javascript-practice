4533e9b9985ee1ef77eb92f3a2eae4eb428a1eb2
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-16 00:09:58 +0530

    intro to arrays

--- 2_blackjack_game/index.js
+++ 2_blackjack_game/index.js
@@ -1,21 +1,39 @@
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
 let firstCard = 4;
 let secondCard = 7;
-
 let sum = firstCard + secondCard;
+
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
 function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
-        hasBlackJack = true;
-    } else {
-        message = "You're out of the game! 😭";
-        isAlive = false;
-    }
+    renderGame();
 }
 
-console.log(message);
+function renderGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    messageEl.textContent = message;
+}
+
+let card = 5;
+
+function newCard() {
+    sum += card;
+
+    renderGame();
+}
